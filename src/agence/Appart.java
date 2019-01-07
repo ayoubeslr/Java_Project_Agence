582d2d12dd130fcc96da145c32e599b85233cbcc
ayoube.soler <ayoube.soler1@gmail.com>
2019-01-07 17:14:39 +0100

    ajout de bienImmobilier

---
+++ src/agence/Appart.java
@@ -0,0 +1,16 @@
+package agence;
+
+public class Appart extends BienImmobilier {
+	int id;
+	String adresse;
+	String orientation;
+	String prixDemande;
+	String dateVenteSouhaiter;
+	int nInterne;
+	
+	public Appart(int id, String adresse, String orientation, String prixDemande, String dateVenteSouhaiter, int nInterne) {
+		
+	}
+	
+
+}
